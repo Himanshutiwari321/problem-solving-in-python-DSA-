3c91cbec657b7da9aacead99ad73e86f466daf3a
himanshutiwari321 <himanshu546686@gmail.com>
2026-01-05 14:30:35 +0530

    update

---
+++ main.py/string_q2.py
@@ -0,0 +1,13 @@
+# tring are pallidrom or not 
+
+s = "madam"
+rev = ""
+
+for i in range(len(s)-1,-1,-1):
+  
+    rev = rev + s[i]
+
+if s==rev:
+  print("palidrom")
+else:
+  print("not palidrom")
