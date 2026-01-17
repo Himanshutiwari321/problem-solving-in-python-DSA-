e209fd0e6d08cb75acb3566a5126dc63d6d94f4b
himanshutiwari321 <himanshu546686@gmail.com>
2026-01-17 16:59:26 +0530

    add

--- main.py/array_q1.py
+++ main.py/array_q1.py
@@ -5,13 +5,29 @@ nums = [2,7,11,15]
 target = 9
 seen = {}
 
-for i in range (len(nums)):
-    cur = nums[i]
-    com = target - cur
+for i in nums:
+    
+    com = target - i
     if com in seen:
        print(seen[com],i)
     else:
-        seen[cur]=i
+        seen[i] = i
+
+
+# print index value == target 
+
+nums = [2,7,11,15]
+target = 9
+seen = {}
+        
+for i in range (len(nums)):
+    cur = nums[i]
+    comm = target - cur 
+    if comm in seen:
+        print(seen[comm],i )
+
+    else:
+        seen[cur] = i        
 
     
     
--- main.py/map_q1.py
+++ main.py/map_q1.py
@@ -1,3 +1,4 @@
+#hasing question ---- how many time m values in n , count and print ---- it just understand the hashing concept 
 n = [1,2,5,5,7,8,7,5,5,10]
 m = [1,75,7,5,111,10]
 
@@ -15,5 +16,31 @@ for i in n:
 for i in m:
     if i in has_map:
        print(i ,"=", has_map[i])
+
+
+
+# for strnig 
+
+s = "azyxyyzaaaa"
+q = ["d", "a","y","z"]
+
+maap = {}
+
+for i in s:
+  if i in maap:
+     maap[i] += 1
+  else:
+      maap[i]= 1
+
+for i in q:
+  if i in maap:
+    print(i, ":", maap[i])
+  else:
+      print (i, ": 0") 
+
+        
+
+
+
     
             
