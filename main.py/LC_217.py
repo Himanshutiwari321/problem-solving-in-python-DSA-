27e7698cc3efacd06ad031e27d90eaa5504e73cb
himanshutiwari321 <himanshu546686@gmail.com>
2026-02-05 15:34:35 +0530

    add

---
+++ main.py/LC_217.py
@@ -0,0 +1,19 @@
+#find duplicate item from list
+
+nums = [1,2,3,22]
+result = {}
+
+for i in nums:
+   if i in result:
+      result[i]+=1
+
+   else:
+      result[i]=1
+
+for i in result:
+   if result[i]!= 1:
+      print(True)
+      break
+else:
+   print(False)  
+      
