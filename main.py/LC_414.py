5cbe1467bf542acc53071778e2b40fc89584cc58
himanshutiwari321 <himanshu546686@gmail.com>
2026-02-12 13:45:00 +0530

    add

---
+++ main.py/LC_414.py
@@ -0,0 +1,26 @@
+#third maximum number in list 
+
+#nums = [1,2]
+nums = [2,2,3,1]
+
+first = second = third = None
+
+for n in set(nums):
+   if first is None or n > first:
+       third = second 
+       second = first 
+       first  = n
+
+   elif second is None or n > second:
+       third = second 
+       second = n
+
+   elif third is None or n > third:
+       third = n    
+
+if third is None :
+    print (first)
+else:
+    print (third)    
+
+
