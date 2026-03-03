fd8259fee8d9b831aef873b54d2e72e87117a870
himanshutiwari321 <himanshu546686@gmail.com>
2026-03-03 18:12:12 +0530

    add

---
+++ main.py/LC_53.py
@@ -0,0 +1,13 @@
+#maximum subarray
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+
+current_sum = nums[0]
+max_sum = nums[0]
+
+for i in range (1,len(nums)):
+           
+    current_sum = max(nums[i] , current_sum + nums[i])
+    max_sum = max (max_sum , current_sum)
+
+print(max_sum)  
