42cc085d2dcff888b633ef405ff1674bb2c7516d
himanshutiwari321 <himanshu546686@gmail.com>
2026-03-12 15:52:54 +0530

    update

--- main.py/greedy_kadane/LC_53.py
+++ main.py/greedy_kadane/LC_53.py
@@ -1,13 +1,32 @@
 #maximum subarray
-
+#bruteforce solution 0(n2)
 nums = [-2,1,-3,4,-1,2,1,-5,4]
-
-current_sum = nums[0]
 max_sum = nums[0]
 
-for i in range (1,len(nums)):
+for i in range (len(nums)):
+    curr_sum = 0
+
+    for j in range (i,len(nums)):
+        curr_sum+=nums[j]
+        if curr_sum>max_sum:
+            max_sum=curr_sum
+       
+print(max_sum)                
+
+
+
+#optimize solution using kadane algorithm 0(n)
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+
+current_sum = 0
+max_sum = nums[0]
+
+for i in range (len(nums)):
            
-    current_sum = max(nums[i] , current_sum + nums[i])
-    max_sum = max (max_sum , current_sum)
+    current_sum +=nums[i]
+    if current_sum>max_sum:
+        max_sum=current_sum
+    if current_sum<0:
+        current_sum = 0    
 
 print(max_sum)  
